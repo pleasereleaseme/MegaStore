5ccf2563f61a84cc8aa771b987c343875757e482
Graham Smith <graham@pleasereleaseme.net>
2018-03-26 23:17:52 +0100

    U

---
+++ MegaStore.SaveSaleHandler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using MegaStore.Helper;
+using NATS.Client;
+
+namespace MegaStore.SaveSaleHandler
+{
+    class Program
+    {
+        private static ManualResetEvent _ResetEvent = new ManualResetEvent(false);
+        private const string QUEUE_GROUP = "save-sale-handler";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Connecting to message queue url: {Env.MessageQueueUrl}");
+            using (var connection = MessageQueue.CreateConnection())
+            {
+                var subscription = connection.SubscribeAsync(SaleCreatedEvent.MessageSubject, QUEUE_GROUP);
+                subscription.MessageHandler += SaveSale;
+                subscription.Start();
+                Console.WriteLine($"Listening on subject: {SaleCreatedEvent.MessageSubject}, queue: {QUEUE_GROUP}");
+
+                _ResetEvent.WaitOne();
+                connection.Close();
+            }
+        }
+
+        private static void SaveSale(object sender, MsgHandlerEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine($"Received message, subject: {e.Message.Subject}");
+                var eventMessage = MessageHelper.FromData<SaleCreatedEvent>(e.Message.Data);
+                Console.WriteLine($"Saving new sale, created at: {eventMessage.CreatedAt}; event ID: {eventMessage.CorrelationId}");
+
+                var sale = eventMessage.Sale;
+
+                sale.SaleID = 10001;
+
+                Console.WriteLine($"Sale saved. Sale ID: {sale.SaleID}; event ID: {eventMessage.CorrelationId}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception processing event: {ex}");
+            }
+        }
+    }
+}
