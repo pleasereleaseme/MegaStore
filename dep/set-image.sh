6d9d95d497c10c72ce8ae2c42a42102a0a65a117
Graham Smith <graham@pleasereleaseme.net>
2018-12-21 13:33:52 +0000

    Update set-iamge

--- dep/set-image.sh
+++ dep/set-image.sh
@@ -1,3 +1,5 @@
-kubectl --kubeconfig $(Agent.TempDirectory)/config set image deployment/megastore-web-deployment megastoreweb=$(AcrName).azurecr.io/megastoreweb:$(Build.BuildNumber) --namespace=$(DatEnvironment)
+#!/bin/bash
 
-kubectl --kubeconfig $(Agent.TempDirectory)/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$(AcrName).azurecr.io/megastoresavesalehandler:$(Build.BuildNumber) --namespace=$(DatEnvironment)
+#kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-web-deployment megastoreweb=$ACRNAME.azurecr.io/megastoreweb:$BUILD_BUILDNUMBER --namespace=$DATENVIRONMENT
+
+kubectl --kubeconfig $1/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$2.azurecr.io/megastoresavesalehandler:$3 --namespace=$4
