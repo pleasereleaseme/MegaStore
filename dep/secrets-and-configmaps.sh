03c9b9ffe8246653f2af6f559d0228f4146c8598
Graham Smith <graham@pleasereleaseme.net>
2018-12-22 14:06:29 +0000

    U

--- dep/secrets-and-configmaps.sh
+++ dep/secrets-and-configmaps.sh
@@ -5,16 +5,16 @@ BUILD_BUILDNUMBER=$3
 ENVIRONMENT=$4
 
 # image pull secret
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $(AcrAuthenticationSecretName) --namespace=$(DatEnvironment) --docker-server=$(AcrName).azurecr.io --docker-username=$(AcrName) --docker-password=$(AcrPassword) --dry-run -o yaml |  kubectl --kubeconfig $(Agent.TempDirectory)/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $(AcrAuthenticationSecretName) --namespace=$(DatEnvironment) --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$(AcrPassword) --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # ASPNETCORE_ENVIRONMENT configmap
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap aspnetcore.env --from-literal=ASPNETCORE_ENVIRONMENT=$(DatEnvironment) --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $(Agent.TempDirectory)/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap aspnetcore.env --from-literal=ASPNETCORE_ENVIRONMENT=$(DatEnvironment) --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # APP_INSIGHTS_INSTRUMENTATION_KEY configmap
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.env --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$(DatAppInsightsInstrumentationKey) --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $(Agent.TempDirectory)/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.env --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$(DatAppInsightsInstrumentationKey) --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # DB_CONNECTION_STRING secret
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="$(DatDbConn)" --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $(Agent.TempDirectory)/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="$(DatDbConn)" --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # MESSAGE_QUEUE_URL configmap
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$(DatEnvironment):4222 --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $(Agent.TempDirectory)/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$(DatEnvironment):4222 --namespace=$(DatEnvironment) --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
