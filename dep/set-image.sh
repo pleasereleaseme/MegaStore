a254b1f198d863c412ca6ba897346250bcdbe08f
Graham Smith <graham@pleasereleaseme.net>
2018-12-22 13:52:32 +0000

    u

--- dep/set-image.sh
+++ dep/set-image.sh
@@ -2,8 +2,8 @@
 AGENT_TEMPDIRECTORY=$1
 ACRNAME=$2
 BUILD_BUILDNUMBER=$3
-DATENVIRONMENT=$4
+ENVIRONMENT=$4
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-web-deployment megastoreweb=$ACRNAME.azurecr.io/megastoreweb:$BUILD_BUILDNUMBER --namespace=$DATENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-web-deployment megastoreweb=$ACRNAME.azurecr.io/megastoreweb:$BUILD_BUILDNUMBER --namespace=$ENVIRONMENT
 
-kubectl --kubeconfig $1/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$2.azurecr.io/megastoresavesalehandler:$3 --namespace=$4
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$ACRNAME.azurecr.io/megastoresavesalehandler:$BUILD_BUILDNUMBER --namespace=$ENVIRONMENT
