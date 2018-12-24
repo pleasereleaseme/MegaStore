131ee1a6716a28352d4179bf8e20be061cec31cd
Graham Smith <graham@pleasereleaseme.net>
2018-12-24 12:42:17 +0000

    U

--- dep/set-image.sh
+++ dep/set-image.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 TAG=$1
-ENVIRONMENT=$2
+ENV=$2
 
-echo "TAG is $TAG"
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-web-deployment megastoreweb=$ACRNAME.azurecr.io/megastoreweb:$TAG --namespace=$ENV
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-web-deployment megastoreweb=$ACRNAME.azurecr.io/megastoreweb:$TAG --namespace=$ENVIRONMENT
-
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$ACRNAME.azurecr.io/megastoresavesalehandler:$TAG --namespace=$ENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$ACRNAME.azurecr.io/megastoresavesalehandler:$TAG --namespace=$ENV
