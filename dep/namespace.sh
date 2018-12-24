25be868d7f728b00484cd03c9a54314a7ca037b4
Graham Smith <graham@pleasereleaseme.net>
2018-12-24 09:25:45 +0000

    u

--- dep/namespace.sh
+++ dep/namespace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ENVIRONMENT=$1
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-build/k8s-config/namespace-$ENVIRONMENT.yaml
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8sbuild/k8s-config/namespace-$ENVIRONMENT.yaml
--- dep/services-and-deployments.sh
+++ dep/services-and-deployments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ENVIRONMENT=$1
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-build/k8s-config/message-queue-service.yaml --namespace=$ENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8sbuild/k8s-config/message-queue-service.yaml --namespace=$ENVIRONMENT
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-build/k8s-config/megastore-web-service.yaml --namespace=$ENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8sbuild/k8s-config/megastore-web-service.yaml --namespace=$ENVIRONMENT
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-build/k8s-config/message-queue-deployment.yaml --namespace=$ENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8sbuild/k8s-config/message-queue-deployment.yaml --namespace=$ENVIRONMENT
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-build/k8s-config/megastore-web-deployment.yaml --namespace=$ENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8sbuild/k8s-config/megastore-web-deployment.yaml --namespace=$ENVIRONMENT
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-build/k8s-config/megastore-savesalehandler-deployment.yaml --namespace=$ENVIRONMENT
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8sbuild/k8s-config/megastore-savesalehandler-deployment.yaml --namespace=$ENVIRONMENT
