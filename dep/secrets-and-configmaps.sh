3ed3b6c8468f271583b9379b64e771eb0c458174
Graham Smith <graham@pleasereleaseme.net>
2018-12-24 11:00:44 +0000

    Sort DB connection string

--- dep/secrets-and-configmaps.sh
+++ dep/secrets-and-configmaps.sh
@@ -2,7 +2,7 @@
 ENVIRONMENT=$1
 ACRPASSWORD=$2
 APPINSIGHTSINSTRUMENTATIONKEY=$3
-DBCONN=$4
+DBPWD=$4
 
 # container registry credentials
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $ACRAUTHENTICATIONSECRETNAME --namespace=$ENVIRONMENT --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$ACRPASSWORD --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
@@ -14,7 +14,7 @@ kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap aspnetcore.env
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.env --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$APPINSIGHTSINSTRUMENTATIONKEY --namespace=$ENVIRONMENT --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # DB_CONNECTION_STRING secret
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="$DBCONN" --namespace=$ENVIRONMENT --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="Server=tcp:megastore.database.windows.net,1433;Initial Catalog=$ENVIRONMENT;Persist Security Info=False;User ID=sales_user_$ENVIRONMENT;Password=$DBPWD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" --namespace=$ENVIRONMENT --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # MESSAGE_QUEUE_URL configmap
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$ENVIRONMENT:4222 --namespace=$ENVIRONMENT --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
