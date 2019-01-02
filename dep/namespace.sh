e8a9117e28b878b1f010ed4d5d133dac3e10af7b
Graham Smith <graham@pleasereleaseme.net>
2019-01-02 21:05:32 +0000

    u

--- dep/namespace.sh
+++ dep/namespace.sh
@@ -2,4 +2,4 @@
 PATH=$1
 ENV=$2
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $PATH/namespace-$ENV.yaml
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f k8s-config-build/k8s-config/namespace-$ENV.yaml
