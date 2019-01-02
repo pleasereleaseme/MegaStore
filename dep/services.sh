ca00d6dba6e1370d68659c368a4f897b630da8c9
Graham Smith <graham@pleasereleaseme.net>
2019-01-02 21:50:18 +0000

    u

--- dep/services.sh
+++ dep/services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PATH=$1
+CONFIGPATH=$1
 ENV=$2
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $PATH/message-queue-service.yaml --namespace=$ENV
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $CONFIGPATH/message-queue-service.yaml --namespace=$ENV
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $PATH/megastore-web-service.yaml --namespace=$ENV
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $CONFIGPATH/megastore-web-service.yaml --namespace=$ENV
