71a797fff26c6a36ee8b6f49b0d6abd7e4f180f1
Graham Smith <graham@pleasereleaseme.net>
2019-01-02 21:27:04 +0000

    u

--- dep/namespace.sh
+++ dep/namespace.sh
@@ -4,4 +4,4 @@ ENV=$2
 
 echo "PATH is $PATH"
 
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $PATH/namespace-$ENV.yaml
+kubectl #--kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $PATH/namespace-$ENV.yaml
