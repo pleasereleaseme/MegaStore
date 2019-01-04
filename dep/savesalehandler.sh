cb95eb04828c6bcedb1af8da710e0d23b7bf9346
Graham Smith <graham@pleasereleaseme.net>
2019-01-04 18:06:50 +0000

    u

--- dep/savesalehandler.sh
+++ dep/savesalehandler.sh
@@ -1,7 +1,10 @@
 CONFIGPATH=$1
 ENV=$2
 
-echo "ENV is $BUILD_BUILDNUMBER"
+echo "TAG is $BUILD_BUILDNUMBER"
+
+# DB_CONNECTION_STRING secret
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="Server=tcp:megastore.database.windows.net,1433;Initial Catalog=$ENV;Persist Security Info=False;User ID=sales_user_$ENV;Password=$DBPWD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # deployment
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $CONFIGPATH/megastore-savesalehandler-deployment.yaml --namespace=$ENV
