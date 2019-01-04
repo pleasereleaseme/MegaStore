c77b6ab4f20075af272b9812d7e49e9471586c09
Graham Smith <graham@pleasereleaseme.net>
2019-01-04 17:52:10 +0000

    u

--- dep/savesalehandler.sh
+++ dep/savesalehandler.sh
@@ -1,9 +1,8 @@
 CONFIGPATH=$1
 ENV=$2
-TAG=$3
 
 # deployment
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $CONFIGPATH/megastore-savesalehandler-deployment.yaml --namespace=$ENV
 
 # set image
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$ACRNAME.azurecr.io/megastoresavesalehandler:$TAG --namespace=$ENV
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config set image deployment/megastore-savesalehandler-deployment megastoresavesalehandler=$ACRNAME.azurecr.io/megastoresavesalehandler:$BUILD_BUILDNUMBER --namespace=$ENV
