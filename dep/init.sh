72f2b6e28f3e473f03216bcfe949d8c8558935da
Graham Smith <graham@pleasereleaseme.net>
2019-01-19 13:30:14 +0000

    script refactoring

--- dep/init.sh
+++ dep/init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-ACRPWD=$1
+ACRPASSWORD=$1
 
 echo "PATH is $K8SCONFIGPATH"
 echo "ENV is $ENV"
 echo "ACRAUTHENTICATIONSECRETNAME is $ACRAUTHENTICATIONSECRETNAME"
 echo "ACRNAME is $ACRNAME"
-echo "ACRPWD is $ACRPWD"
+echo "ACRPASSWORD is $ACRPASSWORD"
 echo "APPINSIGHTSINSTRUMENTATIONKEY is $APPINSIGHTSINSTRUMENTATIONKEY"
 
 # namespace
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $K8SCONFIGPATH/namespace-$ENV.yaml
 
 # container registry credentials
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $ACRAUTHENTICATIONSECRETNAME --namespace=$ENV --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$ACRPWD --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $ACRAUTHENTICATIONSECRETNAME --namespace=$ENV --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$ACRPASSWORD --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # MESSAGE_QUEUE_URL configmap (not used directly by message-queue deployment but configured here as used by several components)
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$ENV:4222 --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
