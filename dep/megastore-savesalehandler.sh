342fdcc6937840c6ce197dd884cdf5b77ffaaeab
Graham Smith <graham@pleasereleaseme.net>
2019-01-19 13:27:45 +0000

    script refactoring

--- dep/megastore-savesalehandler.sh
+++ dep/megastore-savesalehandler.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-DBPWD=$1
+DBPASSWORD=$1
 
 echo "PATH is $K8SCONFIGPATH"
 echo "ENV is $ENV"
-echo "DBPWD is $DBPWD"
+echo "DBPASSWORD is $DBPASSWORD"
 echo "BUILD_BUILDNUMBER is $BUILD_BUILDNUMBER"
 
 # DB_CONNECTION_STRING secret
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="Server=tcp:megastore.database.windows.net,1433;Initial Catalog=$ENV;Persist Security Info=False;User ID=sales_user_$ENV;Password=$DBPWD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret generic db.connection --from-literal=DB_CONNECTION_STRING="Server=tcp:megastore.database.windows.net,1433;Initial Catalog=$ENV;Persist Security Info=False;User ID=sales_user_$ENV;Password=$DBPASSWORD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # deployment
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $K8SCONFIGPATH/megastore-savesalehandler-deployment.yaml --namespace=$ENV
