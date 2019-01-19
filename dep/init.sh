4e7dc46e6d4c5ee204a0a8cb8e0df9e984eaf34e
Graham Smith <graham@pleasereleaseme.net>
2019-01-19 14:51:30 +0000

    script refactoring

--- dep/init.sh
+++ dep/init.sh
@@ -2,20 +2,20 @@
 ACRPASSWORD=$1
 
 echo "PATH is $K8SCONFIGPATH"
-echo "ENV is $ENV"
+echo "RELEASE_ENVIRONMENTNAME is $RELEASE_ENVIRONMENTNAME"
 echo "ACRAUTHENTICATIONSECRETNAME is $ACRAUTHENTICATIONSECRETNAME"
 echo "ACRNAME is $ACRNAME"
 echo "ACRPASSWORD is $ACRPASSWORD"
 echo "APPINSIGHTSINSTRUMENTATIONKEY is $APPINSIGHTSINSTRUMENTATIONKEY"
 
 # namespace
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $K8SCONFIGPATH/namespace-$ENV.yaml
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $K8SCONFIGPATH/namespace-$RELEASE_ENVIRONMENTNAME.yaml
 
 # container registry credentials
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $ACRAUTHENTICATIONSECRETNAME --namespace=$ENV --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$ACRPASSWORD --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $ACRAUTHENTICATIONSECRETNAME --namespace=$RELEASE_ENVIRONMENTNAME --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$ACRPASSWORD --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # MESSAGE_QUEUE_URL configmap (not used directly by message-queue deployment but configured here as used by several components)
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$ENV:4222 --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$RELEASE_ENVIRONMENTNAME:4222 --namespace=$RELEASE_ENVIRONMENTNAME --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # APP_INSIGHTS_INSTRUMENTATION_KEY configmap
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.env --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$APPINSIGHTSINSTRUMENTATIONKEY --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.RELEASE_ENVIRONMENTNAME --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$APPINSIGHTSINSTRUMENTATIONKEY --namespace=$RELEASE_ENVIRONMENTNAME --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
