72b5dcda0c97f4204369fba0361626e5d761c647
Graham Smith <graham@pleasereleaseme.net>
2019-01-19 09:47:01 +0000

    script refactoring

--- dep/init.sh
+++ dep/init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-CONFIGPATH=$1
-#ENV=$2
-ACRPWD=$2
-AIKEY=$3
 
-echo "PATH is $CONFIGPATH"
+echo "PATH is $K8SCONFIGPATH"
 echo "ENV is $ENV"
+echo "ACRAUTHENTICATIONSECRETNAME is $ACRAUTHENTICATIONSECRETNAME"
+echo "ACRNAME is $ACRNAME"
+echo "ACRPWD is $ACRPWD"
+echo "APPINSIGHTSINSTRUMENTATIONKEY is $APPINSIGHTSINSTRUMENTATIONKEY"
 
 # namespace
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $CONFIGPATH/namespace-$ENV.yaml
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f $K8SCONFIGPATH/namespace-$ENV.yaml
 
 # container registry credentials
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $ACRAUTHENTICATIONSECRETNAME --namespace=$ENV --docker-server=$ACRNAME.azurecr.io --docker-username=$ACRNAME --docker-password=$ACRPWD --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
@@ -17,4 +17,4 @@ kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create secret docker-registry $
 kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap message.queue --from-literal=MESSAGE_QUEUE_URL=nats://message-queue-service.$ENV:4222 --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
 
 # APP_INSIGHTS_INSTRUMENTATION_KEY configmap
-kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.env --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$AIKEY --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
+kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config create configmap appinsights.env --from-literal=APP_INSIGHTS_INSTRUMENTATION_KEY=$APPINSIGHTSINSTRUMENTATIONKEY --namespace=$ENV --dry-run -o yaml |  kubectl --kubeconfig $AGENT_TEMPDIRECTORY/config apply -f -
